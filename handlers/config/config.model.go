8a3b1f5d5ee946f64eb27450d90cb16a57010cc5
MurilloSSJ <murillostore@gmail.com>
2024-11-21 19:22:35 -0300

    Update config Type for Database Port to int

--- handlers/config/config.model.go
+++ handlers/config/config.model.go
@@ -3,7 +3,7 @@ package config
 type Config struct {
 	Database struct {
 		Host     string `yaml:"host"`
-		Port     string `yaml:"port"`
+		Port     int `yaml:"port"`
 		Username string `yaml:"username"`
 		Password string `yaml:"password"`
 		Database string `yaml:"database"`
