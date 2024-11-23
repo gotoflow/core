80ed5e090acf143dfa131a9c442c8ac7bae8f737
MurilloSSJ <murillostore@gmail.com>
2024-11-23 12:14:48 -0300

    adjust db driver

--- handlers/database/factory.go
+++ handlers/database/factory.go
@@ -1,16 +1,21 @@
 package database
 
-func GetDatabase(driver string, host string, port string, username string, password string, database string) Database {
+import (
+	"log"
+
+	"gorm.io/gorm"
+)
+
+func GetDatabase(driver string, host string, port string, username string, password string, database string) *gorm.DB {
 	switch driver {
 		case "postgres":
-			return &Postgres{
-				Host:     host,
-				Port:     port,
-				Username: username,
-				Password: password,
-				Database: database,
+			db, err := GetPostgresConnection(host, port, username, password, database)
+			if err != nil {
+				log.Fatal(err)
 			}
+			return db
 		default:
+			log.Fatal("Invalid driver")
 			return nil
 	}
 }
--- handlers/database/postgres.go
+++ handlers/database/postgres.go
@@ -7,54 +7,10 @@ import (
 	"gorm.io/gorm"
 )
 
-type Postgres struct {
-	Host     string
-	Port     string
-	Username string
-	Password string
-	Database string
-	Connection *gorm.DB
-}
-
-func (p *Postgres) GetConnection() error {
+func GetPostgresConnection(host string, port string, username string, password string, database string) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		p.Host, p.Port, p.Username, p.Database, p.Password)
-	if p.Connection != nil {
-		return nil
-	}
+		host, port, username, database, password)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return err
-	}
-	p.Connection = db
-	return nil
-}
-
-func (p *Postgres) CloseConnection() {
-	sqlDB, err := p.Connection.DB()
-	if err != nil {
-		return
-	}
-	sqlDB.Close()
-}
-
-func (p *Postgres) Insert(data interface {}) error {
-	return p.Connection.Create(data).Error
-}
-
-func (p *Postgres) Update(data interface {}) error {
-	return p.Connection.Save(data).Error
-}
-
-func (p *Postgres) Delete(data interface {}) error {
-	return p.Connection.Delete(data).Error
-}
-
-func (p *Postgres) Get(data interface {}) error {
-	return p.Connection.First(data).Error
-}
-
-func (p *Postgres) GetAll(data interface {}) error {
-	return p.Connection.Find(data).Error
+	return db, err
 }
 
